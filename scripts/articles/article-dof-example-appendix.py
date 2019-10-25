9f8da4da82524bf8e6167130a865eac1f0853d70
Pieter Eendebak <pieter.eendebak@gmail.com>
2019-10-25 22:38:48 +0200

    add dof article example

---
+++ scripts/articles/article-dof-example-appendix.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 28 16:43:36 2013
+
+@author: eendebakpt
+"""
+
+#%% Exammple of delete-one-factor article appendix
+import oapackage
+
+al = oapackage.exampleArray(4)
+al = oapackage.reduceDOPform(al)
+al.showarray()
+print('GWLP %s' % str(al.GWLP()))
+for ii in range(0, al.n_columns):
+    bl = al.deleteColumn(ii)
+    print('dof %d: GWLP %s' % (ii, str(bl.GWLP())))
+
+dopgwp = oapackage.projectionGWLPs(al)
+sg = oapackage.symmetry_group(dopgwp, False)
+sg.show(1)
+
+al = oapackage.exampleArray(5)
+al.showarray()
+print('GWLP %s' % str(al.GWLP()))
+
+aldop = oapackage.reduceDOPform(al)
+aldop.showarray()
+
+#import oahelper
+for ii in range(0, al.n_columns):
+    bl = al.deleteColumn(ii)
+    print('delete column %d: GWLP %s' % (ii, str(bl.GWLP())))
+
+aldop = oapackage.reduceDOPform(al, 1)
